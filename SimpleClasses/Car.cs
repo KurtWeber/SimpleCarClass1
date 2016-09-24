800547bed06b6bbaf85e4f9ff62a1c3bb8d9f587
Kurt Weber <weberkrt@msn.com>
2016-09-23 23:06:04 -0400

    Class help to understand

---
+++ SimpleClasses/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Car
+    {
+        // Fields
+        private string make;
+        private string model;
+        private int year;
+        private string color;
+
+        // Properties for each variable
+        public string Make
+        {
+            get { return this.make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return this.model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return this.year; }
+            set { year = value; }
+        }
+        public string Color
+        {
+            get { return this.color; }
+            set { color = value; }
+        }
+
+        // Constructor
+        public Car(string make, string model, int year, string color)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
+        }
+
+        // Method
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+
+            if (Year > 1990)
+            {
+                carValue = 10000;
+            }
+            else
+            {
+                carValue = 2000;
+            }
+            return carValue;
+        }
+    }
+}
---
+++ SimpleClasses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Oldsmobile", "Cutlas Supreme", 1986, "Silver");  //creating an instance "myCar" of the Car class
+            //myCar.Make = "Oldsmobile";        // Video put constructore in Main
+            //myCar.Model = "Cutlas Supreme";
+            //myCar.Year = 1986;
+            //myCar.Color = "Silver";
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color );
+
+            decimal value = myCar.DetermineMarketValue();
+            Console.WriteLine("{0:C}", value);
+
+            //Console.ReadKey();
+
+            //Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+
+        }
+        /*private static decimal DetermineMarketValue(Car car)  // Helper method not needed
+        {
+            decimal carValue = 100.0M;
+
+            //Someday I might look up the value of the car...
+            return carValue;
+        }
+        */
+    }
+}
